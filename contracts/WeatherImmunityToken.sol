75cdb31e8eced3d8cfacde41fdd23b70fd8eeb3a
Ben Andre <benandre@iPod-Classic-3.home>
2018-02-11 05:46:49 -0500

    Create WIT partnerships.

--- contracts/WeatherImmunityToken.sol
+++ contracts/WeatherImmunityToken.sol
@@ -4,9 +4,6 @@ import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 //import './CropToken.sol' as CropToken;
 
-
-
-
 /**
  * @title Weather Immunity Token for crop and weather protection
  *
@@ -15,15 +12,41 @@ import 'zeppelin-solidity/contracts/math/SafeMath.sol';
  */
 contract WeatherImmunityToken is ERC721Token {
 
-    uint256[] tokenIDs; // We keep track of our IDs like good ethereum citizens.
+    
+  // Mapping from token ID to partner token ID
+  mapping (uint256 => uint256) private tokenPartners;
 
-    function CreateToken() public {
+  event CreatedWIT(address owner, uint256 tokenID);
+  event WITsPartnered(uint256 tokenIDOne, uint256 tokenIDTwo);
 
-      
-      _mint(msg.sender, totalSupply()+1);
+    function createWIT(uint256 partnerID) public {
 
+      uint256 newID = totalSupply() + 1; // index 0 never gets used.
+      // TODO check for overflow condition of ^
+
+      // If a partnerID is specified, that token must be unparterned. 
+      // Or 0 can be specified, meaning we are creating an unpartnered token.
+      require(tokenPartners[partnerID] == 0); 
+
+      //other validation happens here
+
+      _mint(msg.sender, newID);
+      CreatedWIT(msg.sender, newID);
+
+      if(partnerID != 0){
+        makePartners(partnerID, newID);
+      }
 
     }
 
-}
+    function makePartners(uint256 firstPartnerID, uint256 secondPartnerID) public {
+      tokenPartners[firstPartnerID] = secondPartnerID;
+      tokenPartners[secondPartnerID] = firstPartnerID;
+      WITsPartnered(firstPartnerID, secondPartnerID);
+    }
 
+    function partnerOf(uint256 tokenID) public view returns (uint256) {
+      return tokenPartners[tokenID]; 
+    }
+
+}
--- test/weather_immunity_token.js
+++ test/weather_immunity_token.js
@@ -2,10 +2,11 @@ var WeatherImmunityToken = artifacts.require("WeatherImmunityToken");
 var expect = require('expect');
 
 contract('WeatherImmunityToken', function(accounts) {
-  it("Should create a token, transfer it to the creator, and increment totalSupply.", function(){
+ /* it("Should create a token, transfer it to the creator, and increment totalSupply.", function(){
     return WeatherImmunityToken.deployed().then(function(instance) {
       WIT = instance;
-      return WIT.CreateToken();
+      console.log(accounts[0]);
+      return WIT.createWIT(0);
     }).then(function(){
       return WIT.totalSupply();
     }).then(function(supply){
@@ -14,5 +15,36 @@ contract('WeatherImmunityToken', function(accounts) {
     }).then(function(owner){
     	assert.equal(owner, accounts[0], "Token was not transferred to it's creator.");
     });
+  });*/
+  it("Should create two tokens, transfer them to the creators, and make the tokens partners", function(){
+    return WeatherImmunityToken.deployed().then(function(instance) {
+    	WIT = instance;
+    	return WIT.createWIT(0, {from: accounts[0]});
+    }).then(function(){
+    	return WIT.createWIT(1, {from: accounts[1]});
+    }).then(function(){
+        return WIT.ownerOf.call(1);
+    }).then(function(owner){
+    	assert.equal(owner, accounts[0], "1st token was not transferred to 1st account");
+    	return WIT.ownerOf.call(2);
+    }).then(function(owner){
+    	assert.equal(owner, accounts[1], "2nd token was not transferred to 2nd account");
+    	return WIT.partnerOf.call(1);
+    }).then(function(partner){
+    	assert.equal(partner.toNumber(), 2, "2nd token is not the partner of 1st");
+    	return WIT.partnerOf.call(2);
+    }).then(function(partner){
+    	assert.equal(partner.toNumber(), 1, "1st token is not the partner of 2nd");
+        return WIT.createWIT(1, {from: accounts[2]});
+    }).then(function(){
+    	return WIT.partnerOf.call(3);
+    }).then(function(partner){
+        assert.notEqual(partner.toNumber(), 1, "Illegal partnership created");
+        return WIT.balanceOf(accounts[2]);
+    }).then(function(balance){
+    	assert.equal(balance, 0, "Illegal token created");
+    });
   });
+
+
 });
