9026625cff6508c7940652764f7fdbf83bf380af
Ben Andre <bandre@gmail.com>
2018-04-05 12:53:45 -0400

    Workin on the NOAA python script.

---
+++ computation-archive/src/NOAAPrecip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import requests
+
+baseURL = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND'
+headers = ''
+
+//construct URL from location 
+
+//convert figure out year of term from start and end args
+
+for (i = current year; i > 
+requests
+
+print("above")
+
+print("below")
