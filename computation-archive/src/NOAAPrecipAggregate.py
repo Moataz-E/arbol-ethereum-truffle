487dc77b342df5a3eae998653a9e3b43dfa4cdcc
Ben Andre <bandre@gmail.com>
2018-04-20 04:13:21 -0400

    Add docstrings to NOAA precipitation aggregate script.

--- computation-archive/src/NOAAPrecipAggregate.py
+++ computation-archive/src/NOAAPrecipAggregate.py
@@ -3,6 +3,12 @@ from bs4 import BeautifulSoup
 
 
 def dictify_vals(raw_values):
+    """
+    Takes as an argument a single string which has key value pairs encoded as
+    individual lines and separated by the "," character.
+
+    Encodes into a dictionary of ints:floats and returns said dictionary.
+    """
     values_dict = {}
     for line in raw_values.contents[0].splitlines():
         try:
@@ -14,6 +20,10 @@ def dictify_vals(raw_values):
 
 
 def calculate_average(start_year, end_year, values_dict):
+    """
+    Takes yearly precipitation data from the dictionary, and returns
+    the average preciptation for inclusive the range between start_year and end_year.
+    """
     sum = 0
     for year in range(start_year, end_year + 1):
         sum += values_dict[year]
@@ -22,9 +32,16 @@ def calculate_average(start_year, end_year, values_dict):
 
     
 def main(timescale, region, month, threshold, term_year, averaged_years):
+    """
+    Hits NOAA's aggregate endpoint (for the given region, month, and timescale), and
+    calculates whether the precipation levels for the term_year exceed the average of
+    previous averaged_years number of years by the given threshold.
+
+    Threshold is a factor of the precipitation level, so "10% above average" is 1.1, and
+    "15% below average" is .85
+    """
     start_year = term_year - averaged_years
     end_year = term_year - 1
-
     result = requests.get("https://www.ncdc.noaa.gov/cag/regional/time-series/mugl.xml?parameter=pcp&timescale=%s&region=%s&month=%s" % (timescale, region, month,))
 
     if result.status_code != 200:
@@ -32,7 +49,7 @@ def main(timescale, region, month, threshold, term_year, averaged_years):
         quit()
 
     soup = BeautifulSoup(result.content, "html.parser")
-    raw_prcp_vals = soup.find("values")
+    raw_prcp_vals = soup.find("values") # "values" is the html element that marks the precipitation data.
     prcp_vals_dict = dictify_vals(raw_prcp_vals)
     avg_prcp = calculate_average(int(start_year), int(end_year), prcp_vals_dict)
     term_prcp = prcp_vals_dict[int(term_year)]
@@ -44,6 +61,12 @@ def main(timescale, region, month, threshold, term_year, averaged_years):
 
 
 def test():
+    """
+    Makes several calls to main.
+
+    Region 261 is corn belt.
+    """
+
     main(timescale=1, region=261, month=3, threshold=1.5, term_year=2017, averaged_years=10)
     main(timescale=1, region=261, month=3, threshold=1.4, term_year=2017, averaged_years=10)
     main(timescale=1, region=261, month=3, threshold=1.3, term_year=2017, averaged_years=10)
