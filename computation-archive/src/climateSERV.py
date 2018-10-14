e867f8e2fa3e8d6f000f641c006c6bcc0bb4566f
Ben Andre <bandre@gmail.com>
2018-10-14 01:14:56 -0400

    Pick NASA's climateSERV for CHIPS HTTPS service.

---
+++ computation-archive/src/climateSERV.py
@@ -0,0 +1,6 @@
+#CHIRPS dataset via climateSERV
+
+#API documentation  https://climateserv.readthedocs.io/en/latest/api.html
+
+#query format {{ base api url }}/[MethodName]/?param1=value1&param2=value2&...paramN=valueN
+
