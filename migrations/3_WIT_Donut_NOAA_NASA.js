89643b6c8e93ec457d21ed99dae8cd251e050208
Ben Andre <bandre@gmail.com>
2018-11-08 10:00:37 -0500

    Few last minute corrections before deploying to Rinkeby.

--- migrations/3_WIT_Donut_NOAA_NASA.js
+++ migrations/3_WIT_Donut_NOAA_NASA.js
@@ -12,9 +12,6 @@ module.exports = function(deployer, network, accounts) {
 	switch(network) {
 		case "development":
 	    	deployer_account = "0x627306090abab3a6e1400e9345bc60c78a8bef57";
-	    	ARBOL.deployed().then(function(AN_ARBOL) {
-	    		arbolcoin_address = AN_ARBOL.address;
-	    	});
 	    	break;
 	    case "rinkeby":
 	        deployer_account = "0x2b8d009b7b256d82a5d3ea679631b69e0d1babeb";
@@ -26,11 +23,16 @@ module.exports = function(deployer, network, accounts) {
 	    	deployer_account = accounts[0];
 	}
 
+	ARBOL.deployed().then(function(AN_ARBOL) {
+	    arbolcoin_address = AN_ARBOL.address;
+	});
+
+
     deployer.deploy([DONUT,WIT,NOAA,NASA], {from: deployer_account}).then(function() {
 		DONUT.deployed().then(function(A_DONUT) {
 			WIT.deployed().then(function(A_WIT) {
 				NOAA.deployed().then(function(A_NOAA) {
-				    NASA.deployed().then(async function(A_NASA) {
+				    NASA.deployed().then(function(A_NASA) {
 						if (network == "development") {
 						    A_NOAA.setLocalOAR();
 						    A_NASA.setLocalOAR();
@@ -40,7 +42,7 @@ module.exports = function(deployer, network, accounts) {
 						A_NASA.transferOwnership(A_WIT.address, {from: deployer_account});
 						A_WIT.initialize(arbolcoin_address, A_DONUT.address, A_NOAA.address, A_NASA.address, {from: deployer_account});
 						web3.eth.sendTransaction({from: deployer_account, to: A_NOAA.address, value:web3.toWei(3, "ether")}); // for oraclize callback.
-						web3.eth.sendTransaction({from: deployer_account, to: A_NASA.address, value:web3.toWei(3, "ether")}); // for oraclize callback.
+						web3.eth.sendTransaction({from: deployer_account, to: A_NASA.address, value:web3.toWei(5, "ether")}); // for oraclize callback.
 					})	
 				})
 			})
--- truffle.js
+++ truffle.js
@@ -10,7 +10,8 @@ module.exports = {
    host: "127.0.0.1",
    port: 8545,
    network_id: "4",
-   gas: 6400000
+   gas: 6400000,
+   from: "0x2b8d009b7b256d82a5d3ea679631b69e0d1babeb"
   },
   mainnet: {
    host: "127.0.0.1",
